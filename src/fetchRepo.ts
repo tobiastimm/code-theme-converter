tobiastimm/code-theme-converter

commit 39d78bd8ff0c6cc75fa2ab202ba44a375a3c66e3

Adding checkout failure detail message (#56)
* catch when git checkout fails

Since more and more repos are using "main" as their base branch instead of "master", just adding this note for newbies like me

* catch when git checkout fails

* adding directory check for ST 3 or ST 4

since ST 4 doesn't use a version number in the folder name anymore

src/idea/convert.ts
@@ -98,5 +98,7 @@ export async function convertToIdea (
     return Promise.all(promises)
   } catch (error) {
     console.trace(chalk.red(error))
+    if(error.includes('checkout')
+      console.log(chalk.red('You may need to add the branch name explicitly to the end of your URL (eg "#main")'))
   }
 }

src/sublime/convert.ts
@@ -86,5 +86,7 @@ export async function convertToSublime (
     return Promise.all(promises)
   } catch (error) {
     console.log(chalk.red(error))
+    if(error.includes('checkout')
+       console.log(chalk.red('You may need to add the branch name explicitly to the end of your URL (eg "#main")'))
   }
 }

src/util/sublime.ts
@@ -1,4 +1,5 @@
 import os from 'os'
+import fs from 'fs'
 
 function getUsername (): string {
   return os.userInfo().username
@@ -8,10 +9,19 @@ export function getSublimeTextPackageDir (): string {
   const username = getUsername()
   switch (os.platform()) {
     case 'win32':
-      return '%APPDATA%/Roaming/Sublime Text 3/Packages/'
+      if(fs.existsSync('%APPDATA%/Roaming/Sublime Text 3/Packages/'))
+        return '%APPDATA%/Roaming/Sublime Text 3/Packages/'
+      else
+        return '%APPDATA%/Roaming/Sublime Text/Packages/'
     case 'darwin':
-      return `/Users/${username}/Library/Application Support/Sublime Text 3/Packages/`
+      if(fs.existsSync(`/Users/${username}/Library/Application Support/Sublime Text 3/Packages/`))
+        return `/Users/${username}/Library/Application Support/Sublime Text 3/Packages/`
+      else
+        return `/Users/${username}/Library/Application Support/Sublime Text/Packages/`
     default:
-      return `/${username}/.config/sublime-text-3/Packages/`
+      if(fs.existsSync(`/${username}/.config/sublime-text-3/Packages/`))
+        return `/${username}/.config/sublime-text-3/Packages/`
+      else
+        return `/${username}/.config/sublime-text/Packages/`
   }
 }

commit 8796a8b5c49638c61bdf0aa2b54e4ec30e3d2838

Fix build and lint issues
- Fix build error introduced by PR #56 - missing closing parentheses
- Fix lint errors introduced by PR #56 - if/else braces
- Fall back to CWD if sublime theme dir not found
- Minor formatting cleanup

src/fetchRepo.ts
@@ -7,7 +7,7 @@ export interface ConverterRepo {
   repo: string
 }
 
-export async function fetchRepo(url: string): Promise<ConverterRepo> {
+export async function fetchRepo (url: string): Promise<ConverterRepo> {
   const tmpRootDir = await createTempDir()
   const tmpRepoDir = path.join(tmpRootDir, 'repo')
 

src/idea/convert.ts
@@ -97,8 +97,14 @@ export async function convertToIdea (
     })
     return Promise.all(promises)
   } catch (error) {
-    console.trace(chalk.red(error))
-    if(error.includes('checkout')
-      console.log(chalk.red('You may need to add the branch name explicitly to the end of your URL (eg "#main")'))
+    const message = error instanceof Error ? error.message : String(error)
+    console.trace(chalk.red(message))
+    if (message.includes('checkout')) {
+      console.log(
+        chalk.red(
+          'You may need to add the branch name explicitly to the end of your URL (e.g. "#main")'
+        )
+      )
+    }
   }
 }

src/sublime/convert.ts
@@ -85,8 +85,14 @@ export async function convertToSublime (
     })
     return Promise.all(promises)
   } catch (error) {
-    console.log(chalk.red(error))
-    if(error.includes('checkout')
-       console.log(chalk.red('You may need to add the branch name explicitly to the end of your URL (eg "#main")'))
+    const message = error instanceof Error ? error.message : String(error)
+    console.log(chalk.red(message))
+    if (message.includes('checkout')) {
+      console.log(
+        chalk.red(
+          'You may need to add the branch name explicitly to the end of your URL (e.g. "#main")'
+        )
+      )
+    }
   }
 }

src/util/sublime.ts
@@ -1,5 +1,6 @@
 import os from 'os'
 import fs from 'fs'
+import chalk from 'chalk'
 
 function getUsername (): string {
   return os.userInfo().username
@@ -7,21 +8,35 @@ function getUsername (): string {
 
 export function getSublimeTextPackageDir (): string {
   const username = getUsername()
+  const testPaths: string[] = []
   switch (os.platform()) {
     case 'win32':
-      if(fs.existsSync('%APPDATA%/Roaming/Sublime Text 3/Packages/'))
-        return '%APPDATA%/Roaming/Sublime Text 3/Packages/'
-      else
-        return '%APPDATA%/Roaming/Sublime Text/Packages/'
+      testPaths.push(
+        '%APPDATA%/Roaming/Sublime Text 3/Packages/',
+        '%APPDATA%/Roaming/Sublime Text/Packages/'
+      )
+      break
     case 'darwin':
-      if(fs.existsSync(`/Users/${username}/Library/Application Support/Sublime Text 3/Packages/`))
-        return `/Users/${username}/Library/Application Support/Sublime Text 3/Packages/`
-      else
-        return `/Users/${username}/Library/Application Support/Sublime Text/Packages/`
+      testPaths.push(
+        `/Users/${username}/Library/Application Support/Sublime Text 3/Packages/`,
+        `/Users/${username}/Library/Application Support/Sublime Text/Packages/`
+      )
+      break
     default:
-      if(fs.existsSync(`/${username}/.config/sublime-text-3/Packages/`))
-        return `/${username}/.config/sublime-text-3/Packages/`
-      else
-        return `/${username}/.config/sublime-text/Packages/`
+      testPaths.push(
+        `/${username}/.config/sublime-text-3/Packages/`,
+        `/${username}/.config/sublime-text/Packages/`
+      )
   }
+
+  for (const testPath of testPaths) {
+    if (fs.existsSync(testPath)) return testPath
+  }
+
+  console.log(
+    chalk.yellow(
+      'Could not find Sublime theme dir, falling back to current working dir'
+    )
+  )
+  return process.cwd()
 }